7c7ba949b09b276e83f7cbad0635584e0d6c65d9
Gerard Dominic Vizcocho <vizcocho.gerarddominic@ue.edu.ph>
2021-09-15 04:21:14 +0800

    Update LandingPage.jsx

--- src/components/customer/LandingPage.jsx
+++ src/components/customer/LandingPage.jsx
@@ -60,53 +60,90 @@ function LandingPage() {
                     );
                 })}
             </Marquee>
-            <div className="row justify-content-center bg-black text-white p-5">
-                <div className="col-lg-4 d-flex flex-column p-3 ">
-                    <h1 className="pb-3 border-gold-3">
-                        YOUR PURE END-TO-END CABLE TV PROVIDER
-                    </h1>
-                    <p className="fs-5">
-                        Upgrade your online experience at home, in your
-                        business, or in the corporate world.
-                    </p>
-                    <p className="fs-5">
-                        Get the plan that you deserve with Lake Community Cable
-                        TV.
-                    </p>
-                    <Link
-                        to="/about"
-                        className="btn btn-warning btn-lg btn-gold-2 mt-auto"
+            <div className="row bg-dark-navy text-white">
+                <div className="m-0 p-0">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1440 320"
                     >
-                        Our Story
-                    </Link>
+                        <path
+                            fill="#FFFFFF"
+                            fill-opacity="1"
+                            d="M0,160L40,170.7C80,181,160,203,240,186.7C320,171,400,117,480,90.7C560,64,640,64,720,69.3C800,75,880,85,960,80C1040,75,1120,53,1200,74.7C1280,96,1360,160,1400,192L1440,224L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+                        ></path>
+                    </svg>
                 </div>
-                <div className="col-lg-4 d-flex flex-column p-3 mt-5 mt-lg-0">
-                    <h1 className="pb-3 border-gold-3">
-                        BE A PART OF LAKE COMMUNITY CABLE TV
-                    </h1>
-                    <p className="fs-5">gagawin ni jester pangit -cho</p>
-                    <Link
-                        to="/plans"
-                        className="btn btn-warning btn-lg btn-gold-2 mt-auto"
-                    >
-                        Apply Now!
-                    </Link>
+                <div className="col-12 p-4 px-5">
+                    <div className="row">
+                        <div className="col-xxl-6 mx-auto">
+                            <h1 className="pb-3 border-gold-3">
+                                YOUR PURE END-TO-END CABLE TV PROVIDER
+                            </h1>
+                            <p className="fs-5">
+                                Upgrade your online experience at home, in your
+                                business, or in the corporate world.
+                            </p>
+                            <p className="fs-5">
+                                Get the plan that you deserve with Lake
+                                Community Cable TV.
+                            </p>
+                            <Link
+                                to="/about"
+                                className="btn btn-warning btn-lg btn-gold-2 mt-auto"
+                            >
+                                Our Story
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-xxl-6 mx-auto">
+                            <h1 className="pb-3 border-gold-3">
+                                BE A PART OF LAKE COMMUNITY CABLE TV
+                            </h1>
+                            <p className="fs-5">
+                                Binge-watch up to 176 channels (High Definition
+                                and Standard Definition) for as low as
+                                P640/month!
+                            </p>
+                            <Link
+                                to="/plans"
+                                className="btn btn-warning btn-lg btn-gold-2 mt-auto"
+                            >
+                                Apply Now!
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-xxl-6 mx-auto">
+                            <h1 className="pb-3 border-gold-3">
+                                WHY CHOOSE LAKE COMMUNITY CABLE TV?
+                            </h1>
+                            <p className="fs-5">
+                                We deliver end-to-end Pure Fiber Optic
+                                connection to empower your home and business
+                                with seamless internet experience, so you
+                                experience better.
+                            </p>
+                            <Link
+                                to="/services"
+                                className="btn btn-warning btn-lg btn-gold-2 mt-auto"
+                            >
+                                Our Services
+                            </Link>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-lg-4 d-flex flex-column p-3 mt-5 mt-lg-0">
-                    <h1 className="pb-3 border-gold-3">
-                        WHY CHOOSE LAKE COMMUNITY CABLE TV?
-                    </h1>
-                    <p className="fs-5">
-                        We deliver end-to-end Pure Fiber Optic connection to
-                        empower your home and business with seamless internet
-                        experience, so you experience better.
-                    </p>
-                    <Link
-                        to="/services"
-                        className="btn btn-warning btn-lg btn-gold-2 mt-auto"
+                <div className="m-0 p-0">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1440 320"
                     >
-                        Our Services
-                    </Link>
+                        <path
+                            fill="#f8f9fa"
+                            fill-opacity="1"
+                            d="M0,256L120,229.3C240,203,480,149,720,144C960,139,1200,181,1320,202.7L1440,224L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+                        ></path>
+                    </svg>
                 </div>
             </div>
         </div>
