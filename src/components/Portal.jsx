6d2463132fc166e29943ff1ced4ebbd8ef7c54dc
JesterRegual <60926532+JesterRegual@users.noreply.github.com>
2021-09-24 01:51:38 +0800

    ll

--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -13,8 +13,8 @@ function Portal() {
     return (
         <Switch>
             <Route exact path="/portal">
-                <div className="container-fluid text-white p-5 mt-5">
-                    <div className="row mt-5">
+                <div className="container-fluid text-white">
+                    <div className="row">
                         <div className="col-lg-6 col-sm-12 mx-auto text-center border-navy-1">
                             <img
                                 src={logo}
@@ -30,17 +30,33 @@ function Portal() {
                             </p>
                         </div>
                     </div>
-                    <div className="row mt-5">
-                        <Card title="ADMIN" path="admin" name="admin" />
-                        <Card title="CASHIER" path="cashier" name="cashier" />
-                        <Card title="CEO" path="ceo" name="ceo" />
-                        <Card
-                            title="FRONT DESK"
-                            path="frontdesk"
-                            name="front"
-                        />
-                        <Card title="JO PERSONNEL" path="joborder" name="jo" />
-                        <Card title="TECHNICIAN" path="tech" name="tech" />
+                    <div className="row mt-5 pt-5 p-0 bg-login">
+                        <div className="col pt-5 mb-5">
+                            <div className="row pb-5 mb-5 justify-content-center">
+                                {/* <Card title="ADMIN" path="admin" name="admin" />
+                                <Card
+                                    title="CASHIER"
+                                    path="cashier"
+                                    name="cashier"
+                                /> */}
+                                <Card title="CEO" path="ceo" name="ceo" />
+                                <Card
+                                    title="FRONT DESK"
+                                    path="frontdesk"
+                                    name="front"
+                                />
+                                {/* <Card
+                                    title="JO PERSONNEL"
+                                    path="joborder"
+                                    name="jo"
+                                />
+                                <Card
+                                    title="TECHNICIAN"
+                                    path="tech"
+                                    name="tech"
+                                /> */}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Route>
