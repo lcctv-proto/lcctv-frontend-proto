58f64233a6b7d8b9c4dbea204c571faff3dafaf7
Gerard Dominic Vizcocho <vizcocho.gerarddominic@ue.edu.ph>
2021-11-22 17:20:10 +0800

    Update TemplateModal.jsx

--- src/components/technician/TemplateModal.jsx
+++ src/components/technician/TemplateModal.jsx
@@ -1,6 +1,5 @@
 import Modal from "react-bootstrap/Modal";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
